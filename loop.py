8fbae9df392f5c963400f04baf3f6c38b7154697
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 18:06:02 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,24 +1,38 @@
 import os
 import time
-import vlc
+import cv2
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class Handler(FileSystemEventHandler):
     def __init__(self):
-        self.vlc_instance = vlc.Instance()
-        self.player = self.vlc_instance.media_player_new()
+        self.video = None
+
+    def play_video(self, path):
+        self.video = cv2.VideoCapture(path)
+        cv2.namedWindow('Video', cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        while self.video.isOpened():
+            ret, frame = self.video.read()
+            if ret:
+                cv2.imshow('Video', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+        self.video.release()
+        cv2.destroyAllWindows()
 
     def on_modified(self, event):
         if event.is_directory:
             return None
         elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
-            if self.player.is_playing():
-                self.player.stop()
-            media = self.vlc_instance.media_new(event.src_path)
-            self.player.set_media(media)
-            self.player.set_fullscreen(True)
-            self.player.play()
+            if self.video and self.video.isOpened():
+                self.video.release()
+                cv2.destroyAllWindows()
+            self.play_video(event.src_path)
 
 if __name__ == "__main__":
     event_handler = Handler()
