af1af02493e8dddce9ed8741675d4b329e53458d
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 17:52:53 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,86 +1,34 @@
 import os
+import time
 import vlc
-from PIL import Image
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import time
 
-# Path to desktop directory
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+class Handler(FileSystemEventHandler):
+    def __init__(self):
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
 
-# No-video image URL and filename
-no_video_image_url = "bit.ly/sand-pi-png"
-no_video_image_filename = "upload.png"
+    def on_modified(self, event):
+        if event.is_directory:
+            return None
+        elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
+            if self.player.is_playing():
+                self.player.stop()
+            media = self.vlc_instance.media_new(event.src_path)
+            self.player.set_media(media)
+            self.player.set_fullscreen(True)
+            self.player.play()
 
-# VLC instance
-vlc_instance = vlc.Instance()
-player = vlc_instance.media_player_new()
+if __name__ == "__main__":
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, path=os.path.expanduser('~/Desktop'), recursive=False)
+    observer.start()
 
-# Flag to indicate no video image is playing
-no_video_playing = True
-
-
-class FileSystemHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        global no_video_playing
-        if event.is_file and event.src_path.endswith(".mp4") or event.src_path.endswith(".avi"):
-            # New video file detected, stop image and start playing
-            print(f"New video file detected: {event.src_path}")
-            if no_video_playing:
-                player.stop()
-                no_video_playing = False
-                play_videos(desktop_path)
-
-
-def play_videos(directory):
-    # Get list of video files
-    print(f"Getting list of video files in: {directory}")
-    videos = [f for f in os.listdir(directory) if f.endswith(".mp4") or f.endswith(".avi")]
-
-    # Loop through videos and play them
-    for video in videos:
-        print(f"Playing video: {video}")
-        player.set_mrl(os.path.join(desktop_path, video))
-        player.play()
-        player.set_fullscreen(True)
-
-        # Wait until video finishes playing
-        while player.get_state() != vlc.State.Ended:
-            vlc.libvlc_video_output_set_window(vlc_instance, player, 0)
-            player.play()
-
-
-def download_no_video_image():
-    """Downloads the no-video image if it doesn't exist."""
-    print(f"Checking for no-video image: {no_video_image_filename}")
-    if not os.path.exists(os.path.join(desktop_path, no_video_image_filename)):
-        print(f"Downloading no-video image: {no_video_image_url}")
-        os.system(f"curl -L -o {os.path.join(desktop_path, no_video_image_filename)} {no_video_image_url}")
-
-
-# Download the no-video image if needed
-download_no_video_image()
-
-# Start watching for new files on desktop
-event_handler = FileSystemEventHandler()
-observer = Observer()
-observer.schedule(event_handler, desktop_path, recursive=False)
-observer.start()
-
-# Check for existing videos and play image if needed
-if not os.listdir(desktop_path):
-    # No videos found, play no-video image
-    print(f"No videos found, playing no-video image: {no_video_image_filename}")
-    player.set_mrl(os.path.join(desktop_path, no_video_image_filename))
-    player.play()
-    player.set_fullscreen(True)
-    no_video_playing = True
-
-try:
-    while True:
-        print(f"Checking for new files...")
-        time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-    player.stop()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
     observer.join()
