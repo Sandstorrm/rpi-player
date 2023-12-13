b150b3d1d0bc23093fe204dc9bf396b731113ff9
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 21:03:28 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -5,30 +5,37 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class Handler(FileSystemEventHandler):
-  def __init__(self):
-    self.vlc_instance = vlc.Instance()
-    self.player = self.vlc_instance.media_player_new()
+    def __init__(self):
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
 
-  def on_modified(self, event):
-    if event.is_directory:
-      return None
-    elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
-      if self.player.is_playing():
+    def on_modified(self, event):
+        if event.is_directory:
+            return None
+        elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
+            if self.player.is_playing():
+                self.player.stop()
+            media = self.vlc_instance.media_new(event.src_path)
+            self.player.set_media(media)
+            self.player.set_fullscreen(True)
+            self.player.play()
+
+    def on_finished(self, event):
+        # Called when the video playback is finished
         self.player.stop()
-      media = self.vlc_instance.media_new(event.src_path)
-      self.player.set_media(media)
-      self.player.set_fullscreen(True)
-      self.player.play()
+        self.player.set_media(self.vlc_instance.media_new(event.src_path))
+        self.player.play()
 
 if __name__ == "__main__":
-  event_handler = Handler()
-  observer = Observer()
-  observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
-  observer.start()
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
+    observer.start()
 
-  try:
-    while True:
-      time.sleep(1)
-  except KeyboardInterrupt:
-    observer.stop()
-  observer.join()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
