3fd4193b04f9c27ae63d1a3ab54da7155f3c2217
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 20:44:45 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,28 +1,31 @@
 import os
 import time
-import subprocess
+import vlc
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class Handler(FileSystemEventHandler):
     def __init__(self):
-        self.process = None
-
-    def play_video(self, path):
-        if self.process:
-            self.process.terminate()
-        self.process = subprocess.Popen(['bv', path])
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_list_player_new()
 
     def on_modified(self, event):
         if event.is_directory:
             return None
         elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
-            self.play_video(event.src_path)
+            if self.player.is_playing():
+                self.player.stop()
+            time.sleep(1)  # wait for the file to be completely written
+            media = self.vlc_instance.media_new(event.src_path)
+            media_list = self.vlc_instance.media_list_new([event.src_path])  # create a new MediaList
+            self.player.set_media_list(media_list)  # set the player's media list
+            self.player.set_playback_mode(vlc.PlaybackMode.loop)  # set playback mode to loop
+            self.player.play()
 
 if __name__ == "__main__":
     event_handler = Handler()
     observer = Observer()
-    observer.schedule(event_handler, path=os.path.expanduser('~/Desktop'), recursive=False)
+    observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
     observer.start()
 
     try:
