17657c87e72ed3c93f5f9c8ed558a22c10e440ee
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 20:53:03 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -7,29 +7,40 @@ from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def __init__(self):
         self.vlc_instance = vlc.Instance()
-        self.list_player = self.vlc_instance.media_list_player_new()
         self.player = self.vlc_instance.media_player_new()
+        self.current_file = None
 
     def on_modified(self, event):
         if event.is_directory:
             return None
         elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
-            if self.list_player.is_playing():
-                self.list_player.stop()
-            time.sleep(5)  # increase the delay
-            try:
-                media = self.vlc_instance.media_new(event.src_path)
-                media_list = self.vlc_instance.media_list_new([event.src_path])  # create a new MediaList
-                self.list_player.set_media_list(media_list)  # set the player's media list
-                self.list_player.set_playback_mode(vlc.PlaybackMode.loop)  # set playback mode to loop
-                self.player.set_fullscreen(True)  # set fullscreen
-                self.list_player.play()
-            except Exception as e:
-                print(f"Failed to play {event.src_path} due to {str(e)}")
+            self.current_file = event.src_path
+            if self.player.is_playing():
+                self.player.stop()
+
+            # Wait for the video to finish uploading
+            while not os.path.isfile(self.current_file):
+                time.sleep(1)
+
+            # Play the video
+            media = self.vlc_instance.media_new(self.current_file)
+            self.player.set_media(media)
+            self.player.set_fullscreen(True)
+            self.player.play()
+
+    def on_ended(self, event):
+        if event.media.get_path() == self.current_file:
+            # Loop the video
+            self.player.set_position(0)
+            self.player.play()
 
 if __name__ == "__main__":
     event_handler = Handler()
     observer = Observer()
+
+    # Add on_ended event listener to loop the video
+    observer.schedule(event_handler, event_handler.on_ended, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
+
     observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
     observer.start()
 
