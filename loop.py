4dd8b327a1a6b18eeb278741cf1d3e21848ab7c7
Sandstorrm <105359227+Sandstorrm@users.noreply.github.com>
2023-12-12 21:24:21 -0500

    Update loop.py

--- loop.py
+++ loop.py
@@ -4,35 +4,32 @@ import vlc
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-class Handler(FileSystemEventHandler):
-  def __init__(self):
-    self.vlc_instance = vlc.Instance()
-    self.player = self.vlc_instance.media_player_new()
+while True:
+    class Handler(FileSystemEventHandler):
+    def __init__(self):
+        self.vlc_instance = vlc.Instance()
+        self.player = self.vlc_instance.media_player_new()
 
-  def on_modified(self, event):
-    if event.is_directory:
-      return None
-    elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
-      if self.player.is_playing():
-        self.player.stop()
-      media = self.vlc_instance.media_new(event.src_path)
-      self.player.set_media(media)
-      self.player.set_fullscreen(True)
-      self.player.play()
+    def on_modified(self, event):
+        if event.is_directory:
+        return None
+        elif event.src_path.endswith(('.mp4', '.mov', '.avi')):
+        if self.player.is_playing():
+            self.player.stop()
+        media = self.vlc_instance.media_new(event.src_path)
+        self.player.set_media(media)
+        self.player.set_fullscreen(True)
+        self.player.play()
 
-if __name__ == "__main__":
-  while True:
+    if __name__ == "__main__":
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
+    observer.start()
+
     try:
-      event_handler = Handler()
-      observer = Observer()
-      observer.schedule(event_handler, path=os.path.expanduser('/home/sand/Desktop'), recursive=False)
-      observer.start()
-
-      while True:
+        while True:
         time.sleep(1)
     except KeyboardInterrupt:
-      observer.stop()
-      observer.join()
-      break
-    except Exception as e:
-      print(f"An error occurred: {e}. Retrying...")
+        observer.stop()
+    observer.join()
